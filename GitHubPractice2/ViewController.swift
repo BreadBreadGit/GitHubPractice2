b40bbadf98515b2ca4fe1e8f2b40cbf9e38348b9
Omar Khan <okhan4519@stu.d214.org>
2022-04-06 10:37:02 -0500

    defined two methods,

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,9 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    var animalArray = ["dog", "cat", "hamster"]
+    
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
