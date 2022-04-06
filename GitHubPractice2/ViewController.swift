6dca85877fb5a187104339c6e167ffe196a84bc6
John <jjackowski4185@stu.d214.org>
2022-04-06 10:19:58 -0500

    Initial Commit

---
+++ GitHubPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice2
+//
+//  Created by John Jackowski on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
